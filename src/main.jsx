9a19a9933824f4efe1015af6365dec9c3c629c17
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-18 23:58:00 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -43,7 +43,6 @@ const router = createBrowserRouter(
         path="/:cateId/posts/:postId"
         element={<PostDetail />}
       />
-      
     </Route>
   )
 );
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // run on github with /helixapp-frontend-jsx-2/
-  // base: "/helixapp-frontend-jsx-2/",
+  base: "/helixapp-frontend-jsx-2/",
 })
