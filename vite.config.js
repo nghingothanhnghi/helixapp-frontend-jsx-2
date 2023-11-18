6ac0c04085b3d2ee5130563e86fb3842b00fcafd
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-18 21:39:41 +0700

    upate

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // run on github with /helixapp-frontend-jsx-2/
-  // base: "/helixapp-frontend-jsx-2/",
-})
