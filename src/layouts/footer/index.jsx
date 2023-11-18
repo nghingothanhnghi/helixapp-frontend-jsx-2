af7eb7d746abf6c25ae56508c542ff2e902bc82e
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-19 00:09:11 +0700

    update

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <footer className="bg-white dark:bg-gray-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between text-center">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://helix/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://helix/" className="hover:underline">Helix™</a>. All Rights Reserved.
                     </span>
                 </div>
             </footer>
